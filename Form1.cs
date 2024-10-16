8f42105a3d52a51c4a3b84a6fc0421369a4ea6f7
leave me alone <authaikhang123@gmail.com>
2024-10-16 18:30:37 +0700

    init

---
+++ Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Db4objects.Db4o;
+using Db4objects.Db4o.Linq;
+
+namespace BTL
+{
+    public partial class Form1 : Form
+    {
+        public IObjectContainer db;
+        public Form1()
+        {
+            InitializeComponent();
+            db = Db4oFactory.OpenFile("Test LINQ");
+            db.Ext();
+        }
+        public void textchange(object sender, EventArgs e)
+        {
+            String name = txt1.Text;
+            IEnumerator<User> result = from User p in db.Query<User>()
+        }
+    }
+    public class User
+    {
+        public User()
+        {
+        }
+
+        public User(string username, string password)
+        {
+            this.username = username;
+            this.password = password;
+        }
+
+        public String username { get; set; }
+        public String password { get; set; }
+
+
+    }
+}
